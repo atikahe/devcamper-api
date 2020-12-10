c1c6131ef9af7b3653b701fa549e0b0224fd1009
Atikah <Atikah@aia.com>
2020-12-10 16:28:47 +0700

    Create middleware

---
+++ src/middleware/logger.js
@@ -0,0 +1,15 @@
+const time = new Date();
+
+/**
+ * @description Logs request to console
+ */
+const logger = (req, res, next) => {
+  const logTime = time.toDateString();
+  const logRequest = `${req.method} ${req.protocol}://${req.get('host')}${req.url}`;
+  const logResponse = `${res.statusCode}`;
+
+  console.log(logTime, logRequest, logResponse);
+  next();
+}
+
+module.exports = logger;
