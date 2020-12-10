e6181d720d15069e630fcbdb7f11a534983eabf0
Atikah <Atikah@aia.com>
2020-12-10 20:17:36 +0700

    Add body parser

--- src/server.js
+++ src/server.js
@@ -14,6 +14,9 @@ connectDB();
 // Load route files
 const bootcamps = require('./routes/bootcamps');
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
