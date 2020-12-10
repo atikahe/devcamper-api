43722f749effef254a0baf620978b62599af0d78
Atikah <Atikah@aia.com>
2020-12-10 23:46:40 +0700

    Use httpAdapter in geocoder

--- src/utils/geocoder.js
+++ src/utils/geocoder.js
@@ -2,6 +2,7 @@ const NodeGeocoder = require('node-geocoder');
 
 const options = {
   provider: process.env.GEOCODER_PROVIDER,
+  httpAdapter: 'https',
   apiKey: process.env.GEOCODER_API_KEY,
   formatter: null
 }
