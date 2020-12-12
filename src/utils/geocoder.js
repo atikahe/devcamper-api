aabe3d98ad52d40f56bdd6ab2996a8831f84ca0b
Atikah <Atikah@aia.com>
2020-12-12 15:57:29 +0700

    Fix geocoder

--- src/utils/geocoder.js
+++ src/utils/geocoder.js
@@ -2,8 +2,8 @@ const NodeGeocoder = require('node-geocoder');
 
 const options = {
   provider: process.env.GEOCODER_PROVIDER,
-  httpAdapter: 'https',
   apiKey: process.env.GEOCODER_API_KEY,
+  httpAdapter: 'http',
   formatter: null
 }
 
