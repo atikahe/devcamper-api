28698ec99602fc4550e65513c2837edef435d16f
Atikah <Atikah@aia.com>
2020-12-16 22:28:57 +0700

    Setup course route

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,11 +3,22 @@ const router = express.Router({ mergeParams: true });
 
 // Load controllers
 const { 
-  getCourses
+  getCourses,
+  getCourseById,
+  createCourse,
+  updateCourse,
+  deleteCourse
 } = require('../controllers/courses');
 
 router
   .route('/')
   .get(getCourses)
+  .post(createCourse)
+
+router
+  .route('/:id')
+  .get(getCourseById)
+  .put(updateCourse)
+  .delete(deleteCourse)
 
 module.exports = router;
