349305252d61bc7fa877ff12e7735946d930313c
Atikah <Atikah@aia.com>
2020-12-16 22:28:29 +0700

    Setup CRUD function for course

--- src/controllers/courses.js
+++ src/controllers/courses.js
@@ -32,3 +32,81 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     data: courses
   });
 });
+
+/**
+ * @description Get course by Id
+ * @route GET /api/v1/courses/:id
+ * @access Public
+ */
+exports.getCourseById = asyncHandler(async (req, res, next) => {
+  const course = await Course.findOne({ _id: req.params.id });
+  if (!course) {
+    next(
+      ErrorResponse(`Course not found`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    msg: `Showing data of ${course.name}`,
+    data: course
+  })
+});
+
+/**
+ * @description Create new course
+ * @route POST /api/v1/courses
+ * @access Private
+ */
+exports.createCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.create(req.body);
+  res.status(201).json({
+    success: true,
+    msg: `New data of ${course.name} is created`,
+    data: course
+  })
+});
+
+/**
+ * @description Update course by Id
+ * @route PUT /api/v1/course/:id
+ * @access Prvate
+ */
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course not found with id ${req.params.id}`, 400)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    msg: `${course.name} data is updated!`,
+    data: course
+  });
+});
+
+/**
+ * @description Delete course by Id
+ * @route DELETE /api/v1/course/:id
+ * @access Private
+ */
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const course = await Course.findOne({ _id: req.params.id });
+
+  if (course < 1) {
+    return next(
+      new ErrorResponse(`Deletion failed, resource not found`, 400)
+    );
+  }
+
+  course.remove();
+  res.status(200).json({
+    success: true,
+    msg: `Resource deleted`,
+  });
+});
