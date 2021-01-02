b95f17468cd3a5c6218f7f5a4176bdf0bb8433cd
Atikah <Atikah@aia.com>
2021-01-02 21:38:29 +0700

    Setup auth controller and send token in cookie

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('../middleware/async');
-const User = require('../models/User')
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
 
 /**
  * @description Register new user
@@ -14,11 +15,67 @@ exports.register = asyncHandler(async (req, res, next) => {
     email,
     password,
     role
-  })
+  });
+
+  sendTokenResponse(user, 200, res);
+});
+
+/**
+ * @description Login user
+ * @route POST /api/v1/auth/login
+ * @access Public
+ */
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide with email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const isMatch = user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  sendTokenResponse(user, 200, res);
+});
+
+/**
+ * @description Get current logged in user
+ * @route GET /api/v1/auth/me
+ * @access Private
+ */
+exports.getMyData = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
 
   res.status(200).json({
     success: true,
-    count: user.length,
     data: user
   })
 })
+
+// Get token from model, create cookie, and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.generateSignedToken();
+  const expiryDays = process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000;
+  const options = {
+    expires: new Date(Date.now() + expiryDays),
+    httpOnly: true
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token
+    })
+}
